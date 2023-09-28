d53ece9dde72c1d39bc1b62135e175f03a26be45
tjay1760 <mcjthiongo@gmail.com>
2023-09-28 13:01:48 +0300

    Add rspec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.all
   end
 
-  def show
+  def show; end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
+RSpec.describe 'users', type: :request do
+  describe 'GET /index', type: :request do
+    before(:example) { get '/users' }
+
+    it 'displays a list of users' do
       expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('Here is a list of users<')
+    end
+  end
+  describe 'GET /show', type: :request do
+    user = User.create(
+      name: 'test user',
+      photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+      bio: 'test_bio',
+      posts_counter: 1
+    )
+    before(:example) { get "/users/#{user.id}" }
+
+    it 'displays the user details for a given user' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('Here is a List of the user details')
     end
   end
 end
