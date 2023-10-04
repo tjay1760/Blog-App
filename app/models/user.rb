be01334bbe59d333725d9b49b4bd267ce61d1cce
tjay1760 <mcjthiongo@gmail.com>
2023-10-04 14:06:01 +0300

    Add users to application

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :likes
 
   after_create :recent_posts
-  validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  # validates :name, presence: true
+  # validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
